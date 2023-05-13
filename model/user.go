0393b7221b43bd7a93fa7efb6a7d8909f8057dcc
adityarizkyramadhan <adityarizky1020@gmail.com>
2023-05-13 14:19:58 +0700

    ✨ feat(user): models database

--- model/user.go
+++ model/user.go
@@ -1,1 +1,18 @@
 package model
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID          uuid.UUID      `gorm:"type:char(36);primaryKey" json:"id"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	Name        string         `json:"name"`
+	PhoneNumber string         `json:"phone_number"`
+	Password    string         `json:"-"`
+}
